8919c4c0ae2626aff4ecf8ca8004f870b9495e17
Christoph Heldeis <chris.heldeis@gmx.de>
2026-02-23 16:05:19 +0200

    Update login UI function

--- ShamrockRemoteAgent.ClientTester/Views/LoginRequestView.xaml.cs
+++ ShamrockRemoteAgent.ClientTester/Views/LoginRequestView.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using ShamrockRemoteAgent.ClientTester.ViewModels;
+using ShamrockRemoteAgent.TCPProtocol.Enums.Payloads.LoginReq;
 using System.Windows;
 using System.Windows.Controls;
 
@@ -27,6 +28,27 @@ namespace ShamrockRemoteAgent.ClientTester.Views
                 }
 
                 vm.SendLoginRequest();
+            }
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (serviceTypeSelect.SelectedItem == null)
+                return;
+
+            // If ServiceType is enum
+            if (serviceTypeSelect != null && OtherServiceText != null)
+            {
+                if (serviceTypeSelect.SelectedItem.ToString() == "OTHER_SERVICE")
+                {
+                    OtherServiceText.IsEnabled = true;
+                    OtherServiceText.Focus();
+                }
+                else
+                {
+                    OtherServiceText!.IsEnabled = false;
+                    OtherServiceText!.Text = string.Empty; // optional clear
+                }
             }
         }
     }
